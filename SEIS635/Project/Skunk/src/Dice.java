27e2c951a98fe14f2cd5559720a4e2a997f8d17d
yemmene <yemmene@gmail.com>
2018-03-16 22:49:09 -0500

    Updates Phase_!

--- SEIS635/Project/Skunk/src/Dice.java
+++ SEIS635/Project/Skunk/src/Dice.java
@@ -1,31 +1,36 @@
-import java.util.*;
+// Represents a combination of two die
+public class Dice {
 
-public class Dice
-{
+    // two die
+    private Die die1, die2;
 
-	private int numofsides;
-	private int faceValue;
-	
-	public Dice(int numberofsides)		//constructor
-	{
-		numofsides = numberofsides;
-		
-		Random myRandomGenerator = new Random();		
-		faceValue = myRandomGenerator.nextInt(numofsides) +1;
-		//end of contractor
-	}
-	
-	//This method will simulate rolling the dice object
-	public void rollDice()
-	{
-		Random myRandomGenerator = new Random();		
-		faceValue = myRandomGenerator.nextInt(numofsides) +1;
-		//end of method rollDice()
-	//This method will return the current face value of the dice objects	
-	}
-	public int getFaceValue()
-	{
-		return faceValue;
-		//end of method getFaceValue
+    
+    // creates two die with two passed die
+    public Dice(Die die1, Die die2) {
+	this.die1 = die1;
+	this.die2 = die2;
+    }
+    
+    // Getters 
+
+    public Die getDie1() {
+        return die1;
+    }
+
+    public Die getDie2() {
+        return die2;
+    }
+
+    // roll both die and return the title
+    public int roll(){
+	die1.roll();
+	die2.roll();
+	return total();
+    }
+
+	int total() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
+    
